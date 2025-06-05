ad04b1127b955c6f6beabfa6301ab37602fe377b
Chan <beforeapril.co@gmail.com>
2025-06-06 00:02:12 +0900

    router versioning

--- server/lambda.js
+++ server/lambda.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", apiRouter);
+app.use("/v1", apiRouter);
 
 app.get("/health", (req, res) => {
     res.json({ status: "ok", message: "Lambda function is running" });
