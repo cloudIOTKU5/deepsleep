7ebc0edae2d715cb6cdb6c8bbde53d52cfa99e64
Chan <beforeapril.co@gmail.com>
2025-06-09 20:46:50 +0900

    lambda cors 설정

--- server/lambda.js
+++ server/lambda.js
@@ -9,7 +9,13 @@ const { connectToRDS } = require("./data/rds-config");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
